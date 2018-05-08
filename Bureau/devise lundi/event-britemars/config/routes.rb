4d78051b5d3cc42c21b2c9e27dee2d4db34a6d19
sam13008 <ikherbane.samir@gmail.com>
2018-05-08 16:06:59 +0200

    f

--- Bureau/devise lundi/event-britemars/config/routes.rb
+++ Bureau/devise lundi/event-britemars/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 resources :devise
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+get "user/:id" , to: "events#userpage" , as: "profil"
   as :users do
   get 'login', to: "events#userpage"
 end
